ac7d423208962e3c07365a0a28dc37a9927843f9
Evelina_Pyechkurova <evelina.pyechkurova@gmail.com>
2024-11-01 12:38:23 +0200

    Added invalid symbols validation in teacher

--- utils/validators/teacherValidators.js
+++ utils/validators/teacherValidators.js
@@ -4,6 +4,13 @@ function notEmptyString(str){
 
 const emptyStringMessage = "field cannot be empty or consist of only whitespace";
 
+function isLetterString(str) {
+    const letterPattern = /^[\p{L}]+$/u;
+    return str != null && typeof str === "string" && letterPattern.test(str.trim());
+}
+
+const invalidLetterMessage = "field must contain only letters (no spaces, numbers, or special characters)";
+
 function validPhoneNumber(number){
     const phoneNumberPattern = /^\+?[1-9]\d{0,2}[-.\s]?(\(?\d{2,4}\)?)[-.\s]?\d{3,4}[-.\s]?\d{3,4}$/;
     return notEmptyString(number) && phoneNumberPattern.test(number);
@@ -30,8 +37,12 @@ function validateCreateTeacher(teacher) {
 
     if (!notEmptyString(name)) 
         errors.name = "Name " + emptyStringMessage;
+    if (!isLetterString(name))
+        errors.name = "Name " + invalidLetterMessage;
     if (!notEmptyString(surname)) 
         errors.surname = "Surname " + emptyStringMessage;
+    if (!isLetterString(surname))
+        errors.surname = "Surname " + invalidLetterMessage;
     if (!validPhoneNumber(phone))
         errors.phone = "Phone " + invalidPhoneNumberMessage;
     if (!validEmail(email)) 
@@ -50,11 +61,11 @@ function validateUpdateTeacher(teacher) {
     if (invalidFields.length > 0)
         errors.invalidFields = `Invalid fields in the update: ${invalidFields.join(", ")}`;
 
-    if ("name" in teacher && !notEmptyString(teacher.name))
-        errors.name = "Name " + emptyStringMessage;
+    if ("name" in teacher && (!notEmptyString(teacher.name) || !isLetterString(teacher.name)))
+        errors.name = "Name " + emptyStringMessage + ", " + invalidLetterMessage;
 
-    if ("surname" in teacher && !notEmptyString(teacher.surname))
-        errors.surname = "Surname " + emptyStringMessage;
+    if ("surname" in teacher && (!notEmptyString(teacher.surname) || !isLetterString(teacher.surname)))
+        errors.surname = "Surname " + emptyStringMessage + ", " + invalidLetterMessage;
 
     if ("phone" in teacher && !validPhoneNumber(teacher.phone))
         errors.phone = "Phone " + invalidPhoneNumberMessage;
@@ -68,6 +79,8 @@ function validateUpdateTeacher(teacher) {
 module.exports = {
     notEmptyString,
     emptyStringMessage,
+    isLetterString,
+    invalidLetterMessage,
     validPhoneNumber,
     invalidPhoneNumberMessage,
     validEmail,
